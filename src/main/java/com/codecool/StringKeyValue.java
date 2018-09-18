c3d60dfaa2d1d1d9457fe065b66bed5d89d7ca83
edobkowski <edobkowski@gmail.com>
2018-09-18 09:35:42 +0200

    Implement StringKeyValue class

---
+++ src/main/java/com/codecool/StringKeyValue.java
@@ -0,0 +1,28 @@
+package com.codecool;
+
+class StringKeyValue {
+
+    private String key;
+    private String value;
+
+    StringKeyValue(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return this.key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
