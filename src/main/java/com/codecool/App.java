ef926afa3830c56fedd3a79ccfc405030d24dab6
edobkowski <edobkowski@gmail.com>
2018-09-18 14:56:28 +0200

    Implement simple example of App busisness logic

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -1,9 +1,42 @@
 package com.codecool;
 
-public class App 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args) {
+        try {
+            StringHashTable correctWords = new StringHashTable(new DefaultHasher());
+            Path wordsListPath = new File("assets/wordlist.txt").toPath();
+            List<String> words = Files.readAllLines(wordsListPath);
+
+            for(String word : words) {
+                correctWords.add(word, word);
+            }
+
+            SpellChecker spellChecker = new SpellChecker(correctWords);
+
+            Path testWordsPath = new File("assets/big-test.txt").toPath();
+            List<String> testWords = Files.readAllLines(testWordsPath);
+
+            for(String word : testWords) {
+                Set<String> suggestions = spellChecker.check(word);
+                if(!suggestions.isEmpty()) {
+                    System.out.println("\nWord not found: " + word.toUpperCase());
+                    System.out.println("Perhabs you meant:\n");
+                    for(String suggestion : suggestions) {
+                        System.out.println(suggestion.toUpperCase());
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
