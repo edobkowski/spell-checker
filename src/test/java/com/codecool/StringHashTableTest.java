b80b2f8529c45c15befb55d83037fd86619273b1
edobkowski <edobkowski@gmail.com>
2018-09-18 09:59:32 +0200

    Implement test cases for adding new entries

---
+++ src/test/java/com/codecool/StringHashTableTest.java
@@ -0,0 +1,61 @@
+package com.codecool;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringHashTableTest {
+
+    private StringHashTable hashTable;
+
+    @BeforeEach()
+    void initializeHashTable() {
+        hashTable = new StringHashTable(3, new DefaultHasher);
+    }
+
+
+    @Test
+    void addTest_SingleCorrectInput() {
+        hashTable.add("Dog", "dog");
+        String expectedResult = "dog";
+        String actualResult = hashTable.get("Dog");
+        int expectedSize = 1;
+        int actualSize = hashTable.size();
+
+        assertEquals(expectedSize, actualSize);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void addTest_MultipleCorrectInput() {
+        hashTable.add("Dog", "dog");
+        hashTable.add("Cat", "cat");
+        hashTable.add("Fish", "fish");
+        List<String> expectedResult = Arrays.asList("dog cat fish");
+        List<String> actualResult = hashTable.getAll();
+        int expectedSize = 3;
+        int actualSize = hashTable.size();
+
+        assertEquals(expectedSize, actualSize);
+        assertIterableEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void addTest_MultipleOverCapacityInput() {
+        hashTable.add("Dog", "dog");
+        hashTable.add("Cat", "cat");
+        hashTable.add("Fish", "fish");
+        hashTable.add("Snake", "snake");
+        List<String> expectedResult = Arrays.asList("dog cat fish snake");
+        List<String> actualResult = hashTable.getAll();
+        int expectedSize = 4;
+        int actualSize = hashTable.size();
+
+        assertEquals(expectedSize, actualSize);
+        assertIterableEquals(expectedResult, actualResult);
+    }
+}
